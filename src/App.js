92b20c6009b2ff30268ce667eec284c448999b35
AshokYadav <yashoka51@gmail.com>
2024-01-22 10:15:40 +0530

    added context api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Headers from "./components/Layout/Headers";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 
 
@@ -12,13 +13,14 @@ function App() {
     setCartItem((value)=> !value);
   }
   return (
-    <>
+    <CartProvider>
       {cartItem &&<Cart  onCartItemHandler={cartItemHandler}/>}
       <Headers onCartItemHandler={cartItemHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
+    
   );
 }
 
